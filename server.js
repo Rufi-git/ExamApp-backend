89d10e5ae5ac5a257b8e3e2dfbb147809904a02a
Rufi-git <rufialiyev2005@gmail.com>
2023-08-16 07:58:40 -0700

    sasadasd

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(
     cors({
-        origin: ["https://riyaziyyat.vercel.app", "http://localhost:5173"],
+        origin: ["https://riyaziyyat.vercel.app","https://examonline.vercel.app", "http://localhost:5173"],
         credentials: true
     })
 )
